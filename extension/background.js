3997fabdbe0b56a3f69a94e7b8080c5b5567bbb8
A91y <ayush.agr254@gmail.com>
2024-06-16 22:09:18 +0530

    updated js script

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,17 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ key: "hello world" }, () => {
-    console.log("Value is set to 'hello world'");
+    console.log("Extension installed");
+    
+    chrome.storage.local.set({ key: "hello world" }, () => {
+      if (chrome.runtime.lastError) {
+        console.error("Error setting value:", chrome.runtime.lastError);
+      } else {
+        console.log("Value is set to 'hello world'");
+        
+        // Verify the value is set correctly
+        chrome.storage.local.get('key', (result) => {
+          console.log("Stored value:", result.key);
+        });
+      }
+    });
   });
-});
+  
